c6602cff434a908224d8a2384f796dd6f6b119e4
aneeshraman17mar@gmail.com <aneeshti123>
2020-10-18 17:30:17 +0530

    updated automation for image plus text!

--- whatsappsendmessageremake.py
+++ whatsappsendmessageremake.py
@@ -2,6 +2,15 @@ import pyautogui
 from time import sleep
 from random import randint
 
+
+def typeMessageAndEnter(message):
+    pyautogui.typewrite(message)
+    pyautogui.keyDown('alt')
+    pyautogui.keyDown('enter')
+    pyautogui.keyUp('enter')
+    pyautogui.keyUp('alt')
+
+
 pyautogui.keyDown('alt')
 pyautogui.keyDown('tab')
 pyautogui.keyUp('tab')
@@ -15,17 +24,25 @@ for contact_element in contact_list:
     pyautogui.keyUp('ctrl')
     pyautogui.keyUp('v')
     pyautogui.hotkey('delete')
-    sleep(randint(1, 15))
+    sleep(randint(1, 3))
     pyautogui.typewrite(contact_element)
-    sleep(randint(1, 15))
+    sleep(randint(1, 3))
     pyautogui.hotkey('enter')
-    sleep(randint(1, 15))
+    sleep(randint(1, 3))
     pyautogui.click(698, 1011)
-    sleep(randint(1, 15))
+    sleep(randint(1, 3))
     pyautogui.keyDown('ctrl')
     pyautogui.keyDown('v')
     pyautogui.keyUp('ctrl')
     pyautogui.keyUp('v')
-    sleep(randint(1, 15))
-    pyautogui.typewrite("*Experience meditation* by registering for this free session at https://tiny.cc/whymeditate1 - see you soon! Please also pass it on to family and friends who may benefit from meditation")
+    sleep(randint(1, 4))
+    typeMessageAndEnter("We request the pleasure of your company at the launch event of iGROW.")
+    typeMessageAndEnter("*17th Oct, 5 PM*")
+    typeMessageAndEnter("Here is the direct link for ease of access : http://tiny.cc/iGROW")
+    typeMessageAndEnter("")
+    typeMessageAndEnter("Warmly")
+    typeMessageAndEnter("Divya Bajaj")
+    typeMessageAndEnter("www.igrowdisc.com")
+    pyautogui.typewrite("_Microgreens Simplified_")
     pyautogui.hotkey("enter")
+
